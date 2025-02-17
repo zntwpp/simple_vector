1ee27d7631c51d6bc2cd58e994831249267099d4
zntwpp <yarikshcher@gmail.com>
2025-02-17 14:03:43 +0300

    added rvalue push back and fix bugs

---
+++ readme.md
@@ -0,0 +1,1 @@
+idk i was just bored, and thats why i did it
--- vect.cpp
+++ vect.cpp
@@ -17,7 +17,11 @@ public:
         memcpy(m_data, arr, size*sizeof(T));
         m_arr_size = size;
     }
+    Vctr(){
+        m_data = new T [m_capacity];
+    }
     ~Vctr(){
+        m_data = nullptr;
         delete[] m_data;
     }
     //in theory it is fast operation
@@ -26,22 +30,54 @@ public:
             resize();
         }
         m_data[m_arr_size] = e;
-        ++m_arr_size;
+        ++m_arr_size; 
     }
+    //rvalue adding element
+    void add_rvalue_elem(T&& e){
+        if(m_arr_size == m_capacity){
+            resize();
+        }
+        m_data[m_arr_size] = std::move(e);
+        ++m_arr_size; 
+    }
+
+    void clear(){
+        m_capacity = 1;
+        m_arr_size = 0;
+        m_data = nullptr;
+        delete[] m_data;
+        m_data = new T [m_capacity];
+    }
+
+    T operator[](int index){
+        return m_data[index];
+    }
+
     T* get(){
         return m_data;
     }
+
     int get_size(){
         return m_arr_size;
+    }
+
+    void print_vctr(){
+        for(int i = 0; i < m_arr_size; ++i){
+            std::cout << m_data[i] << " ";
+        }
+        std::cout << "\n";
     }
 private:
     T* m_data = nullptr;
     unsigned int m_arr_size = 0;
     size_t m_capacity = 1;
+
     void resize(){
         m_capacity *= 2;
+        std::cout << "resizing, capacity = " << m_capacity << "\n";
         T* new_data = new T [m_capacity];
         memcpy(new_data, m_data, m_capacity*sizeof(T));
+        m_data = nullptr;
         delete[] m_data;
         m_data = new_data;
     }
@@ -52,11 +88,22 @@ public:
     void frst_test(){
         v.add_elem(9);
         v.add_elem(8);
-        if(v.get()[5] == 9 && v.get()[6] == 8){
+        if(v[5] == 9 && v[6] == 8){
             std::cout << "first pass\n";
         }
         else{
             std::cout << "first failed\n";
+        }
+    }
+    void scnd_test(){
+        v.clear();
+        v.add_rvalue_elem(5);
+        v.add_rvalue_elem(8);
+        if(v[0] == 5 && v[1] == 8){
+            std::cout << "second pass\n";
+        }
+        else{
+            std::cout << "second failed\n";
         }
     }
 
@@ -66,6 +113,7 @@ private:
 };
 
 int main(){
-    Unit_tests test;
-    test.frst_test();
+    Unit_tests tests;
+    tests.frst_test();
+    tests.scnd_test();
 }
